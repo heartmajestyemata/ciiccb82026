c848c13708e83598cae1185198e8291c54b40401
heartmajestyemata <heartmajestyemata@gmail.com>
2026-07-23 06:46:28 +0000

    Task/Task2.java submission

---
+++ Task2.java
@@ -0,0 +1,17 @@
+public class Task2{
+    public static void main(String[] args){
+        char letterH = 'H';
+        byte three = 3;
+        short one = 1;
+        int zero = 0;
+        float two = 2.0f;
+        boolean truth = true;
+
+        String output = letterH + "" + three + one + one + zero +
+                        " w" + zero + "r" + one + "d " +
+                        two + " " + truth;
+
+        System.out.println(output);
+    }
+}
+    
